d6c1e21a5177abbfc78410956da36388ca3fa9f8
josemoralesmc <josemorales270102@gmail.com>
2021-09-18 18:31:02 -0300

    a

--- public/js/login.js
+++ public/js/login.js
@@ -32,7 +32,7 @@ async function Ingresar(e) {
     console.log(response);
      if (response.ok) {
         localStorage.setItem("Token", JSON.stringify(res));
-       window.location.href = "index.html";
+       window.location.href = "body.html";
         
     } else {
        alert("Usuario y/o contraseña incorrecta")
